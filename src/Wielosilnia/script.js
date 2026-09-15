783aeeda859fd646981e0ad0a276e9d8e4a7f9af
Eul3r <eul3r@kali>
2026-09-15 11:24:57 +0200

    Fixed typos and implemented multifactorial computing

--- src/Wielosilnia/script.js
+++ src/Wielosilnia/script.js
@@ -1,35 +1,71 @@
+function multifactorial(n, k) {
+    // Check whether n and k are integers
+    if (!Number.isInteger(n) || !Number.isInteger(k)) {
+        return NaN;
+    }
+
+    // Check for invalid values
+    if (k < 0 || k > n) {
+        return 0n;
+    }
+    n = BigInt(n);
+    k = BigInt(k);
+    let r = BigInt(n % k);
+    let result = 1n;
+    for(let i = 1n; i <= BigInt(n); i++)
+    {
+        if(i % k === r)
+            result *= i;
+    }
+
+    return result;
+}
+
+
+
 (() => {
-const formularz = document.getElementById("silniaForm");
-const poleLiczby = document.getElementById("liczba");
+const formularz = document.getElementById("multifactorialForm");
+const pole_n = document.getElementById("liczban");
+const pole_k = document.getElementById("liczbak");
 const wynik = document.getElementById("wynik");
 
 formularz.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    const wartosc = poleLiczby.value;
+    const wartosc1 = pole_n.value;
+    const wartosc2 = pole_k.value;
+
 
     // Sprawdzenie, czy pole nie jest puste
-    if (wartosc === "") {
-        wynik.textContent = "Błąd: wprowadź liczbę.";
+    if (wartosc1 === "" || wartosc2 === "") {
+        wynik.textContent = "Błąd: wprowadź liczby";
         return;
     }
 
-    const n = Number(wartosc);
+    const n = Number(wartosc1);
+    const k = Number(wartosc2);
 
     // Sprawdzenie, czy liczba jest całkowita i nieujemna
     if (!Number.isInteger(n) || n < 0) {
-        wynik.textContent = "Błąd: wprowadź liczbę naturalną.";
+        wynik.textContent = "Błąd: n musi być liczbą naturalną(!)";
+        return;
+    }
+    if (!Number.isInteger(k) || k <= 0) {
+        wynik.textContent = "Błąd: k musi być liczbą całkowitą dodatnią(!)";
         return;
     }
 
-    // Obliczanie silni za pomocą BigInt
-    let silnia = 1n;
+    // if (k > n) {
+    //     wynik.textContent = "Błąd: k nie może być większe od n (k ≤ n)";
+    //     return;
+    // }
 
-    for (let i = 2n; i <= BigInt(n); i++) {
-        silnia *= i;
+    const result = multifactorial(n, k);
+    console.log(result);
+    // Wyświetlenie wyniku i ponowne renderowanie MathJax
+    wynik.innerHTML = `\(\\(${n}!)_{${k}} = ${result.toString()}\\)`;
+    if (window.MathJax && MathJax.typesetPromise) {
+        MathJax.typesetPromise([wynik]);
     }
-
-    // Wyświetlenie wyniku
-    wynik.textContent = `${n}! = ${silnia}`;
 });
 })();
